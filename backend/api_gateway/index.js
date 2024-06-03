476a86d4d32e2e43361d4bb098fd096a7d48cf6c
limashou <poliatskyi.maksym@gmail.com>
2024-06-03 20:01:41 +0300

    fix and update

--- backend/api_gateway/index.js
+++ backend/api_gateway/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const session = require('express-session');
 const {createProxyMiddleware} = require('http-proxy-middleware');
 const proxyRouter = require("./routers/proxyRouter");
 const helmet = require("helmet");
@@ -15,6 +16,14 @@ app.use(cors({
     origin: ['http://localhost:3000', 'http://172.27.96.1:3000', 'http://192.168.1.3:3000'],
     credentials: true,
 }));
+
+app.use(
+    session({
+        secret: 'session secret',
+        resave: false,
+        saveUninitialized: true
+    })
+);
 
 app.use(rateLimitAndTimeout);
 app.use(fileUploadMiddleware);
